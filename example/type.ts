8ead7c7f72edd6c888163c177dfa42dc08e07280
Edward-1020 <iezzuyoung@gmail.com>
2019-06-23 14:36:27 +0800

    feat: interface

--- example/type.ts
+++ example/type.ts
@@ -35,3 +35,63 @@ create(null);
 let someValue: any = 'this is a string';
 // let strLength: number = (<string>someValue).length;
 let strLength:number = (someValue as string).length;
+function sangangsi () {
+    var a = 10;
+}
+let input = [1, 2];
+let [first, second] = input;
+function f([first, second]: [number, number]) {}
+let [,,third] = [1, 2, 3];
+let o = {
+    a: 1,
+    b: 2
+};
+let {a: newName1, b: newName2} = o;
+let {...d} = o;
+function keepWholeObject(wholeObject: {a: string, b?: number}) {
+    let {a, b = 1001} = wholeObject;
+}
+function f1({a, b = 0} = {a: ''}): void {
+}
+f1({a: 'yes'});
+interface LabelledValue {
+    label: string
+}
+function printLabel(labelledObj: LabelledValue) {
+    console.log(labelledObj.label);
+}
+let myObj = {
+    size: 10,
+    label: 'aa'
+}
+printLabel(myObj);
+interface Square {
+    color: string,
+    area: number
+}
+//  可以发现拼写错误
+interface SquareConfig {
+    color?: string,
+    width?: number
+}
+function createSquare(config: SquareConfig): Square {
+    let newSquare = {color: 'red', area: 100};
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+let mySquare = createSquare({
+});
+//  当变量用const，当属性用readonly
+interface Point {
+    readonly x: number;
+    readonly y: number;
+}
+
+let p1: Point = {x: 10, y: 20};
+let a: number[] = [1, 2, 3, 4];
+let ro: ReadonlyArray<number> = a;
