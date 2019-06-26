77977de410d0e414f52ab8d7d7a51a1492862bab
Edward-1020 <iezzuyoung@gmail.com>
2019-06-26 22:47:54 +0800

    feat: 泛型

--- example/type.ts
+++ example/type.ts
@@ -593,4 +593,68 @@ let ponit3d: Point3d = {x1: 1, y1: 2, z1: 3};
     }
 
     let myIdentity1: GericIdentityFn<string> = identity;
+
+    class GenericNumber<T> {
+        zeroValue: T
+        add: (x: T, y: T) => T
+    }
+
+    let myGenericNumber = new GenericNumber<number>();
+    myGenericNumber.zeroValue = 0;
+    myGenericNumber.add = function (x, y) {
+        return x + y;
+    }
+
+    let stringNumeric = new GenericNumber<string>();
+    stringNumeric.zeroValue = '';
+    stringNumeric.add = function (x, y) {
+        return x + y;
+    }
+
+    interface Lengthwise {
+        length: number
+    }
+
+    function loggingIdentity1<T extends Lengthwise>(arg: T): T {
+        console.log(arg.length)
+        return arg;
+    }
+
+    function getProperty<T, K extends keyof T>(obj: T, key: K) {
+        return obj[key]
+    }
+
+    let x = {a: 1, b: 2, c: 3};
+    getProperty(x, 'a');
+
+    function create<T>(c: new() => T): T {
+        return new c();
+    }
+
+    class BeeKeeper {
+        hasMask: boolean
+    }
+
+    class LionKeeper {
+        nametag: string
+    }
+
+    class Animal {
+        numLengs: number
+    }
+
+    class Bee extends Animal {
+        keeper: BeeKeeper
+    }
+
+    class Lion extends Animal {
+        keeper: LionKeeper
+    }
+
+    function createInstance<T extends Animal>(c: new() => T): T {
+        return new c();
+    }
+
+    createInstance(Lion).keeper.nametag;
+    createInstance(Bee).keeper.hasMask;
 })();
